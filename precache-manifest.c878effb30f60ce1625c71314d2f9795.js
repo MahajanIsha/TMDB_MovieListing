7bb2d4c71a77c60af7fcbabdb2b17dee84fce9b6
Isha <mahajanisha87@gmail.com>
2020-06-08 23:07:35 +1000

    Updates

---
+++ precache-manifest.c878effb30f60ce1625c71314d2f9795.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91dcf65cdca4ea7f49663925538d5f1",
+    "url": "/TMDB_MovieListing/index.html"
+  },
+  {
+    "revision": "283325002dd7882fdce9",
+    "url": "/TMDB_MovieListing/static/css/main.0098e4b7.chunk.css"
+  },
+  {
+    "revision": "fb672706e33337c5055d",
+    "url": "/TMDB_MovieListing/static/js/2.71e96bc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TMDB_MovieListing/static/js/2.71e96bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283325002dd7882fdce9",
+    "url": "/TMDB_MovieListing/static/js/main.9ef9169f.chunk.js"
+  },
+  {
+    "revision": "719da78d6fdc303db798",
+    "url": "/TMDB_MovieListing/static/js/runtime-main.c89a88bc.js"
+  },
+  {
+    "revision": "a45a997bd0b90ac78afcc6f19a52f168",
+    "url": "/TMDB_MovieListing/static/media/logo.a45a997b.jpg"
+  },
+  {
+    "revision": "a001764d0784bf2a3b85741d8211cff4",
+    "url": "/TMDB_MovieListing/static/media/rectangle.a001764d.svg"
+  }
+]);
